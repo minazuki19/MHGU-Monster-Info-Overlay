cf28ad02110528905e1207165ded5f8020dd3248
minazuki19 <dev@minazuki.li>
2020-12-20 20:13:03 +0800

    support other languages of game

--- source/monster.cpp
+++ source/monster.cpp
@@ -24,6 +24,29 @@ void initMonsterInfoDB()
 MonsterInfo* getMonsterInfoFromDB(Monster* m)
 {
     u32 id = m->identifier1;
+
+    // offset different from game language
+    switch (id & 0xFF) {
+        // Français
+        // Deutsch
+        // 繁體中文
+        case 0x40:
+        case 0xC0:
+            id = id + 0x60;
+            break;
+        // Español
+        // Italiano
+        case 0x00:
+        case 0x80:
+            id = id + 0x5A0;
+            break;
+        // English
+        // 简体中文
+        case 0x20:
+        case 0xA0:
+        default:
+            break;
+    }
     id <<= 8;
     id += m->identifier2;
     void* result = bsearch(&id, database, num_db_entries, sizeof(MonsterInfo), compareMonsterInfo);
